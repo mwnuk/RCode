f4d040bc7950bde55337f9088499eda5f4fca825
Mark Wnuk <marwnuk@yahoo.com>
2017-01-25 22:20:53 -0500

    created

--- Regression-Lasso.R
+++ Regression-Lasso.R
@@ -8,6 +8,16 @@ bmi_p   <- c(0.86, 0.45, 0.99, 0.84, 0.85, 0.67, 0.91, 0.29, 0.88)
 m_edu   <- as.factor(c(0, 1, 1, 2, 2, 3, 2, 0, 1))
 p_edu   <- as.factor(c(0, 2, 2, 2, 2, 3, 2, 0, 0))
 f_color <- as.factor(c("blue", "blue", "yellow", "red", "red", "yellow", 
+
+
+# age (age of child in years) - continuous
+# gender - binary (1 = male; 0 = female)
+# bmi_p (BMI percentile) - continuous
+# m_edu (mother highest education level) - ordinal (0 = less than high school; 1 = high school diploma; 2 = bachelors degree; 3 = post-baccalaureate degree)
+# p_edu (father highest education level) - ordinal (same as m_edu)
+# f_color (favorite primary color) - nominal ("blue", "red", or "yellow")
+# asthma (child asthma status) - binary (1 = asthma; 0 = no asthma)
+
                        "yellow", "red", "yellow"))
 asthma <- c(1, 1, 0, 1, 0, 0, 0, 1, 1)
 
