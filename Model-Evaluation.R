f13399cb726a4b98316aa3eacadbc5d4a8fa7df8
Mark Wnuk <marwnuk@yahoo.com>
2017-02-08 11:57:22 -0500

    Created

---
+++ Model-Evaluation.R
@@ -0,0 +1,23 @@
+- R2 is the ratio between of the amount of variance explained by a regression model to the total variation of the data.
+  It measures goodness of model.
+  In Finance expect R2 in range 0.5-0.8, im Biology much lower in a range 0.005 -0.007
+  R2=0.6 means thet we reduced outcome variance arount it means by 60%
+- p value - proability that event happens negating  null Hypotesis - less then 0.05 - high probability
+    values greater then 0.05 are not evidence against null hipotesis.
+- t statistic merely measure how strong is the evidence that there is a nonzero association. 
+   Even a weak effect can be extremely significant given enough data. Range 2-40, bigger better.
+   By hand can be done using t-tests( 1-sample t-test, paired t-test,2-sample t-test) 
+-F statistics - The F statistic tests the null hypothesis that none of the predictors has any effect. 
+  Rejecting that null means concluding that *some* predictor has an effect, not that *all* of them do. Range 100-600. bigger better. 
+-ANOVA( Analysis of Variance) - measures F-ratio, it is defined as the ratio of Mean Square(between groups) 
+  to Mean Square(within groups)
+- Chi-square Goodness of fit when outcome is categorical. It is used top calculated p-value and then judgw the null hypothesis.
+
+Is at least one predictor useful?
+ F statistics is large
+
+
+Which predictors are important?
+
+
+How good is the modfel?
