c4af3e21f8cf1410a64af39fd8847abd65a21e5f
Mark Wnuk <marwnuk@yahoo.com>
2017-05-22 17:02:55 -0400

    updated

--- RandomForests.R
+++ RandomForests.R
@@ -1,3 +1,14 @@
+#DECISION TREES
+# - split on Gini Index, Chi-Square or Entropy
+# - pruning or setting constrains on tree size
+# - R packages: ctree, rpart, tree
+#ENSAMBLE METHODS
+# - BAGGING - Bootstrap Sampling - sub-samples
+# - Random Forest - random split decision, good for classification
+# - Boosting - every split improves the chances
+
+https://www.analyticsvidhya.com/blog/2016/04/complete-tutorial-tree-based-modeling-scratch-in-python/
+
 http://trevorstephens.com/kaggle-titanic-tutorial/r-part-5-random-forests/
 
 Random Forests process has the two sources of randomness:
